28842115cc59d83d6b58ef520c2b6743a7b89fd9
adel <adeelguitoun@gmail.com>
2024-05-17 20:31:17 +0100

    Add watch list link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,9 +5,14 @@ const Header = () => {
     <div className="bg-blue-600">
       <div className="container mx-auto text-white">
         <div className="flex justify-between items-center py-8">
-          <Link to="/" className="text-3xl font-bold">
-            MarketStock.io
-          </Link>
+          <div className="flex items-baseline gap-6">
+            <Link to="/" className="text-3xl font-bold">
+              MarketStock.io
+            </Link>
+            <Link to="/watch-list" className="text-xl font-bold underline">
+              Watch list
+            </Link>
+          </div>
           {/* Desktop */}
           <button className="bg-white text-blue-600 font-bold py-2 px-5 rounded-md">
             Log In
