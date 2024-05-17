c3de914228c9212afd9d68a3a213309339f47288
adel <adeelguitoun@gmail.com>
2024-05-17 20:32:23 +0100

    Add stock card and basic styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,16 @@
+import StockCard from '../components/StockCard';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="flex flex-col items-center gap-5 pt-8">
+      <h1 className="text-5xl font-bold text-blue-500">
+        Stock Market MERN app.
+      </h1>
+      <h1 className="text-3xl font-bold text-blue-300">Stocks</h1>
+      <div className="w-full">
+        <StockCard />
+      </div>
+    </div>
+  );
 };
 export default Home;
