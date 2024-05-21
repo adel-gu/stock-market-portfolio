0c35626f4795d5becba91ec3191ad0d0a9d26fda
adel <adeelguitoun@gmail.com>
2024-05-21 22:40:07 +0100

    Add pagination functionality

--- client/src/api/stocks.api.ts
+++ client/src/api/stocks.api.ts
@@ -10,18 +10,23 @@ export interface IStock {
   symbol?: String;
 }
 
+interface IData {
+  stocks?: IStock[];
+  pages?: number;
+}
+
 interface IResponse {
   status: string;
-  data: {
-    stocks: IStock[];
-  };
+  data: IData;
 }
 
 const API_BASE_URL = 'http://localhost:5050/api/v1/' + 'stocks';
 
-const useGetStocks = (query: string): IStock[] | undefined => {
+const useGetStocks = (query: string, page: number): IData | undefined => {
   const getStocksRequest = async (): Promise<IResponse> => {
-    const response = await fetch(`${API_BASE_URL}/?company=${query}`);
+    const response = await fetch(
+      `${API_BASE_URL}/?company=${query}&page=${page}`,
+    );
 
     if (!response.ok) throw new Error('Error fetching all stocks');
 
@@ -29,11 +34,11 @@ const useGetStocks = (query: string): IStock[] | undefined => {
   };
 
   const { data } = useQuery({
-    queryKey: ['getAllStocks', query],
+    queryKey: ['getAllStocks', query, page],
     queryFn: () => getStocksRequest(),
   });
 
-  return data?.data.stocks;
+  return { stocks: data?.data.stocks, pages: data?.data.pages };
 };
 
 export default useGetStocks;
