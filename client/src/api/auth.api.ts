a4a23dba02624b9b35251a11eaef75aa13f5a375
adel <adeelguitoun@gmail.com>
2024-05-22 18:00:43 +0100

    Implement user login functionality

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
-import { SignUpInputsType } from '../pages/SignUp';
 import { useNavigate } from 'react-router-dom';
+import { SignUpInputsType } from '../pages/SignUp';
+import { LoginInputsType } from '../pages/Login';
 
 const API_BASE_URL = 'http://localhost:5050/api/v1/' + 'users';
 
@@ -33,5 +34,37 @@ export const useSignUpUser = () => {
   return {
     isPending,
     signUpUser,
+  };
+};
+
+export const useLoginUser = () => {
+  const navigate = useNavigate();
+  const loginUserRequest = async (data: LoginInputsType) => {
+    const response = await fetch(`${API_BASE_URL}/login`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) throw new Error('Error signing up user');
+  };
+
+  const { mutateAsync: loginUser, isPending } = useMutation({
+    mutationKey: ['loginUser'],
+    mutationFn: loginUserRequest,
+    onSuccess: async () => {
+      navigate('/');
+    },
+    onError: (err: Error) => {
+      console.log(err.message);
+    },
+  });
+
+  return {
+    isPending,
+    loginUser,
   };
 };
