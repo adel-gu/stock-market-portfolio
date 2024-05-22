1ea48be955eab5750f54eabb29613c8b3e9986c4
adel <adeelguitoun@gmail.com>
2024-05-22 11:44:41 +0100

    Add login route

--- server/routes/user.router.ts
+++ server/routes/user.router.ts
@@ -4,5 +4,6 @@ import authController from '../controllers/auth.controller';
 const router = express.Router();
 
 router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login);
 
 export default router;
